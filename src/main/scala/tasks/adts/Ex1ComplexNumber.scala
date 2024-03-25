20558beab9315ad3098757d0c8e20185230a29ed
Elisa Barberini - elisa.barberini3@studio.unibo.it <elisa.barberini3@studio.unibo.it>
2024-03-25 12:00:21 +0100

    task 1 e exercise 4

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -22,11 +22,22 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Real = Double;
+    type Imag = Double;
+
+    type Complex = ComplexImpl;
+    case class ComplexImpl(real : Real, imaginary : Imag);
+
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.real
+      def im(): Double = complex.imaginary
+      def sum(other: Complex): Complex = ComplexImpl(complex.re() + other.re(), complex.im() + other.im() )
+      def subtract(other: Complex): Complex = ComplexImpl(complex.re() - other.re(), complex.im() - other.im() )
+      def asString(): String = complex match
+        case ComplexImpl(re, 0) => complex.re().toString
+        case ComplexImpl(0, im) => complex.im().toString + "i"
+        case ComplexImpl(0, 0) => "0.0"
+        case _ => complex.re().toString + " + " + complex.im().toString + "i"
+
+
---
+++ src/test/scala/tasks/adts/SummableTest.scala
@@ -0,0 +1,11 @@
+package tasks.adts
+
+import org.junit.*
+import org.junit.Assert.*
+import u04lab.Ex1ComplexNumbers.*
+/* Tests should be clear, but note they are expressed independently of the
+   specific implementation
+*/
+
+class SummableTest:
+  val summable = ???
